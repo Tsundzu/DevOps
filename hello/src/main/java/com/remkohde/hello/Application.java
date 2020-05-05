99996ff3eb0a98b7114f2b7acb29995c29f52669
unknown <tsundzumo@gmail.com>
2020-05-05 13:39:15 +0200

    adding a complete spring-helloworld app

---
+++ hello/src/main/java/com/remkohde/hello/Application.java
@@ -0,0 +1,13 @@
+package com.remkohde.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* Entry point or the default main class for our Spring Application */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	    }
+}
